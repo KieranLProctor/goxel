c66213c3770d78f6fae8c5ed21c38dd0e943ddc8
KieranLProctor <kieran.lee.proctor@gmail.com>
2026-04-30 01:09:16 +0100

    chore: cmake now setup correctly

--- dependencies.cmake
+++ dependencies.cmake
@@ -1,11 +1,15 @@
 include(FetchContent)
 
-# IMGUI
-FetchContent_Declare(
-        imgui
-        GIT_REPOSITORY https://github.com/ocornut/imgui.git
-        GIT_TAG v1.92.5-docking)
-FetchContent_MakeAvailable(imgui)
+# OpenGL
+find_package(OpenGL REQUIRED)
+
+# GLAD
+set(GLAD_DIR "${CMAKE_CURRENT_SOURCE_DIR}/external/glad")
+if (NOT TARGET glad)
+    add_library(glad STATIC "${GLAD_DIR}/src/glad.c")
+    target_include_directories(glad PUBLIC "${GLAD_DIR}/include")
+    target_link_libraries(glad PUBLIC OpenGL::GL)
+endif ()
 
 # GLFW
 FetchContent_Declare(
@@ -17,3 +21,30 @@ set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
 set(GLFW_INSTALL OFF CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(glfw)
+
+# ImGUI
+FetchContent_Declare(
+        imgui
+        GIT_REPOSITORY https://github.com/ocornut/imgui.git
+        GIT_TAG v1.92.5-docking)
+FetchContent_MakeAvailable(imgui)
+add_library(imgui STATIC
+        ${imgui_SOURCE_DIR}/imgui.cpp
+        ${imgui_SOURCE_DIR}/imgui_draw.cpp
+        ${imgui_SOURCE_DIR}/imgui_demo.cpp
+        ${imgui_SOURCE_DIR}/imgui_tables.cpp
+        ${imgui_SOURCE_DIR}/imgui_widgets.cpp
+        ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
+        ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp
+)
+target_include_directories(imgui
+        PUBLIC
+        ${imgui_SOURCE_DIR}
+        ${imgui_SOURCE_DIR}/backends
+)
+target_link_libraries(imgui
+        PUBLIC
+        glfw
+        glad
+        OpenGL::GL
+)
